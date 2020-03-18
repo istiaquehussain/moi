d523c5c6b066fa4a710a46ad25edab7063cbb94c
istiaque_hussain <istiaque_hussain@apple.com>
2020-03-18 17:53:31 +0530

    logger added

--- core/src/main/java/com/coe/moi/core/CoreApplication.java
+++ core/src/main/java/com/coe/moi/core/CoreApplication.java
@@ -2,12 +2,16 @@ package com.coe.moi.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class CoreApplication {
-
+	private static final Logger LOGGER = LogManager.getLogger(CoreApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
+		LOGGER.info("Core Application Service  started successfully .....");
 	}
 
 }
